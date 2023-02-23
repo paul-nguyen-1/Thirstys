6efb23ab163b0cfd76ba148d9ecd9475060304b8
paul-nguyen96 <paul121596@icloud.com>
2023-02-22 21:46:48 -0600

    refactor mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from "react";
-import BanList from "./components/BanList";
-import DiscoverList from "./components/DiscoverList";
-import Seen from "./components/Seen";
-import './App.css'
-
+import "./App.css";
 
 function App() {
-  const [submit, setSubmit] = useState("")
+  const BASE_URL = "www.thecocktaildb.com/api/json/v1/";
+  const API_KEY = import.meta.env.API_KEY;
+
+  const makeQuery = () => {};
+
+  const callAPI = () => {};
+  const [submit, setSubmit] = useState("");
   return (
     <div className="App">
-      <Seen />
-      <DiscoverList />
-      <BanList />
+      <div className="seenContainer">
+        <h3>What have we seen so far?</h3>
+      </div>
+      <div className="discoverContainer">
+        <h1>Shots! Shots! Shots!</h1>
+        <h2>Discover the best drinks from your wildest dreams.</h2>
+        <p>🍺🍻🍷🍸🍹🥂🥃🍶🍾</p>
+        <button>Discover! 🔀</button>
+      </div>
+      <div className="banContainer">
+        <h3>Ban List</h3>
+        <h4>Select an attribute in your listing to ban it</h4>
+      </div>
     </div>
   );
 }
