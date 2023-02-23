42f0159eb63cf09c581775bde050f3f514f4cc24
paul-nguyen96 <paul121596@icloud.com>
2023-02-23 11:54:00 -0600

    set attributes to api

--- src/App.jsx
+++ src/App.jsx
@@ -5,33 +5,16 @@ import Shots from "./components/Shots";
 function App() {
   const BASE_URL = "https://www.thecocktaildb.com/api/json/v1/";
   const VITE_API_KEY = import.meta.env.VITE_API_KEY;
-  const [attributes, setAttributes] = useState({
-    strDrink: "ABC",
-    strCategory: "shot",
-    strGlass: "shot glass",
-    strIngredient1: "Amaretto",
-  });
+  const [attributes, setAttributes] = useState(null);
   const [seenImgs, setSeenImgs] = useState([]);
   const [currentImg, setCurrentImg] = useState(null);
 
-  // const makeQuery = () => {
-  //   fetch(`${BASE_URL}${VITE_API_KEY}/random.php`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //     });
-  // };
-
-  // Set up API image once we click button
-  const handleClick = (e) => {
-    // setAttributes((prevState) => ({
-    //   ...prevState,
-    //   [e.target.name]: e.target.value.trim(),
-    // }));
+  // Handle API Data on discover click
+  const handleClick = () => {
     makeQuery();
   };
 
-  //Query set up
+  //Query set up to call API
   const makeQuery = () => {
     let query = `${BASE_URL}${VITE_API_KEY}/random.php`;
     callAPI(query).catch(console.error);
@@ -42,11 +25,16 @@ function App() {
     const response = await fetch(query);
     const json = await response.json();
     if (json.drinks == null) {
-      console.log(json);
       alert("Oops! Something went wrong with that query, let's try again!");
     } else {
       console.log(json.drinks[0].strDrinkThumb);
       setCurrentImg(json.drinks[0].strDrinkThumb);
+      setAttributes({
+        strDrink: json.drinks[0].strDrink,
+        strCategory: json.drinks[0].strCategory,
+        strGlass: json.drinks[0].strGlass,
+        strIngredient1: json.drinks[0].strIngredient1,
+      });
     }
   };
 
